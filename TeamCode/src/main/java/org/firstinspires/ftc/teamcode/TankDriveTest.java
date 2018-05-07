b59f3face695d0b513bd05a1d0607043d0f6fe21
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-05-07 15:47:28 -0600

    Ready to test TankDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTest.java
@@ -1,6 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
-import org.BeehiveRobotics.Library.Util.*;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-public class TankDriveTest {
+import org.BeehiveRobotics.Library.Motors.TankDrive;
+import org.BeehiveRobotics.Library.Util.BROpMode;
+
+@TeleOp(name="TestDrive", group="Test")
+public class TankDriveTest extends BROpMode{
+    double left;
+    double right;
+    TankDrive drive = new TankDrive(this);
+    public void initialize() {
+        setOpModeType(OpModeType.TeleOp);
+        TankDrive drive = new TankDrive(this);
+    }
+    public void run() {
+        left = controller1.leftStickY();
+        right = controller1.rightStickY();
+        drive.drive(left, right);
+    }
+    public void end() {
+        drive.stopMotors();
+    }
 }
