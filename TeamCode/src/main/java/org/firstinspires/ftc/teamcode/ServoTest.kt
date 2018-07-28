d467abe2c6370f573cd7d8b9638f5274bed0c1e2
KadenGordon <kaden.m.gordon@gmail.com>
2018-07-28 15:48:34 -0600

    Update servotest to include speed managing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.kt
@@ -10,8 +10,16 @@ class ServoTest: BROpMode() {
     override fun initialize() {
         servo = Servo(this, "servo")
         servo.position = 0.0
+        servo.speed = 1.0
     }
     override fun run() {
         servo.position = 1.0
+        servo.speed = 0.75
+        servo.position = 0.0
+        servo.speed = 0.25
+        servo.position = 0.5
+        servo.speed = 0.0
+        servo.position = 0.0
+        sleep(300)
     }
 }
