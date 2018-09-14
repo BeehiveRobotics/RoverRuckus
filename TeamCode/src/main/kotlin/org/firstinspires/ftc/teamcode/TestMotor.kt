d80c1f135b7fe8a1e240db8f44b234bfe740f7a6
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-09-13 21:51:38 -0600

    Starting working on CV

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/TestMotor.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/TestMotor.kt
@@ -3,12 +3,17 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.BeehiveRobotics.Library.Util.BROpMode
 import org.BeehiveRobotics.Library.Motors.Motor
+import com.qualcomm.robotcore.hardware.DcMotor
 
 @Autonomous(name="Test Motor", group="Test")
 class TestMotor: BROpMode(OpModeType.Autonomous) {
     private lateinit var motor: Motor
+    private lateinit var dcmotor: DcMotor
+
     override fun initialize() {
         this.motor = Motor(this, "fl")
+        this.dcmotor = hardwareMap.get(DcMotor::class.java, "eh")
+        
     }
     override fun run() {
         this.motor.target = 1120.0 * 10
