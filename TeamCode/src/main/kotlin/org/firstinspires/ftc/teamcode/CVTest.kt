878928ff05b21ce769e56349b52166024992c72c
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-09-28 17:25:50 -0600

    Update CVTest.kt

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/CVTest.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/CVTest.kt
@@ -4,19 +4,20 @@ import org.BeehiveRobotics.Library.Util.BROpMode
 import org.BeehiveRobotics.RoverRuckusCV.Detectors.SampleDetector
 import org.BeehiveRobotics.RoverRuckusCV.CameraViewDisplay
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.util.ElapsedTime
 
 @TeleOp(name="TestCV", group="Test")
-class CVTest: BROpMode(OpModeType.TeleOp) {
+class CVTest: BROpMode(OpModeType.Autonomous) {
     private val cv: SampleDetector = SampleDetector()
     override fun initialize() {
         cv.init(hardwareMap.appContext, CameraViewDisplay.getInstance())
         cv.enable()
     }
     override fun run() {
-        while(opModeIsActive()) {
-        }
+        val runTime = ElapsedTime()
+        while(runTime.seconds()<2){}
+        cv.disable()
     }
     override fun end() {
-        cv.disable()
     }
 }
