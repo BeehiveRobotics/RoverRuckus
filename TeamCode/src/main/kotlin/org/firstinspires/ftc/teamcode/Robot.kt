0ffa18fe2ee9f03b9140100b222451d3cf1f6d94
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-10-19 16:41:59 -0600

    Update Robot to have the 20:1 motors

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Robot.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Robot.kt
@@ -4,12 +4,14 @@ import org.BeehiveRobotics.Library.Util.BROpMode
 import org.BeehiveRobotics.Library.Systems.MecanumDrive
 import com.qualcomm.robotcore.util.ElapsedTime
 import org.BeehiveRobotics.Library.Robots.Robot
+import org.BeehiveRobotics.Library.Motors.Motor
 
 class Robot(private val opMode: BROpMode): Robot(opMode) {
     internal lateinit var drive: MecanumDrive
     internal lateinit var lift: Lift
     override fun init() {
-        drive = MecanumDrive(opMode)
+        drive = MecanumDrive(opMode, gearRatio = 0.5)
+        drive.model = Motor.MotorModel.NEVEREST20
         drive.init()
         lift = Lift(opMode)
     }
