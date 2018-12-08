5edc33fdc5491010bd4130107a8ddb59d3e01576
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-12-08 11:16:02 -0700

    Made a OpMode for testing the CV

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/TestCV.kt
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import org.BeehiveRobotics.Library.Util.BROpMode
+
+@Autonomous(name="Test CV", group="Test")
+class TestCV(): BROpMode(OpModeType.TeleOp) {
+    private val robot = Robot(this)
+    override fun initialize() {
+        robot.init()
+    }
+    override fun onStartPressed() {
+        robot.cv.startCV()
+    }
+    override fun run() {
+        dashboard.addLine(robot.cv.getGoldMineralPosition().toString())
+        dashboard.update()
+    }
+    
+}
