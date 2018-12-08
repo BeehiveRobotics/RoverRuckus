9cb901d2da956cbba16859100588b2d34edd3581
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-12-08 11:17:15 -0700

    Starting to sample in autonomous

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/BlueCrater.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/BlueCrater.kt
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.BeehiveRobotics.Library.Util.BROpMode
+import org.firstinspires.ftc.teamcode.CV.GoldMineralPosition
 
 @Autonomous(name="Blue Crater", group="RR2")
 class BlueCrater(): BROpMode(BROpMode.OpModeType.Autonomous) {
@@ -17,5 +18,18 @@ class BlueCrater(): BROpMode(BROpMode.OpModeType.Autonomous) {
         dashboard.addLine(goldMineralPosition.toString())
         dashboard.update()
         robot.cv.stopCV()
+        robot.drive.forward(0.6, 14.0)
+        when(goldMineralPosition) {
+            GoldMineralPosition.LEFT -> {
+                robot.drive.strafeLeft(1.0, 16.0)
+            }
+            GoldMineralPosition.CENTER -> {
+                robot.drive.strafeRight(1.0, 6.0)
+            }
+            GoldMineralPosition.RIGHT -> {
+                robot.drive.strafeRight(1.0, 24.0)
+            }
+        }
+        robot.drive.forward(1.0, 12.0)
     }
 }
