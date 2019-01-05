f8ff7969ce447553c04a5603ac97e127de83f44d
Kaden Gordon <Kaden.m.gordon@gmail.com>
2019-01-05 14:41:16 -0700

    Update Library

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Lift.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Lift.kt
@@ -6,74 +6,26 @@ import org.BeehiveRobotics.Library.Motors.Motor
 import org.BeehiveRobotics.Library.Servos.Servo
 import com.qualcomm.robotcore.hardware.DcMotor
 
-class Lift(private val opMode: BROpMode): RobotSystem(opMode), Runnable {
+class Lift(private val opMode: BROpMode): RobotSystem(opMode) {
     enum class Tasks {
         CLIMBING, DEPLOYING, NEITHER
     }
-    private var currentTask = Tasks.NEITHER
-    val climbMotor = Motor(opMode, "lcm")
-    val deploymentMotor = Motor(opMode, "ldm")
-
-    val SERVO_LOCK_POSITION = 1.0
-    val SERVO_UNLOCK_POSITION = 0.0
-    var isLocked = true
-
-    var deployingSpeed = 0.0
-    var climbingSpeed = 0.0
-
-    private var deployMotorBrake = false
-
-    override fun init() {
-        climbMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-        climbMotor.rawPower = 0.0
-        climbMotor.MIN_SPEED = 0.4
-        climbMotor.RAMPING_COEFFICIENT = 0.28
-        deploymentMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
-        deploymentMotor.rawPower = 0.0
+    val lockServo = Servo(opMode, "lls")
+    val leftMotor = Servo(opMode, "llm")
+    val rightMotor = Servo(opMode, "lrm")
+    init {
+        leftMotor.direction = com.qualcomm.robotcore.hardware.DcMotor.Direction.REVERSE
     }
-    override fun start() {
-        deploymentMotor.resetEncoder()
-        climbMotor.resetEncoder()
-        if(opMode.opModeType == BROpMode.OpModeType.TeleOp) {
-            val thread = Thread(this)
-            thread.start()
-        }
+    fun move(speed: Double) {
+        leftMotor.power = 
+    }
+    fun move(speed: Double, time: Long) {
+
     }
 
-    override fun toString(): String =
-        "Task: ${currentTask.toString()}\n" + 
-        "Deploying Speed: $deployingSpeed\n" + 
-        "Climbing Speed: $climbingSpeed\n" + 
-        "Deploying Motor Speed: ${deploymentMotor.rawPower}\n" + 
-        "Climbing Motor Speed: ${climbMotor.rawPower}"
+    fun move(speed: Double, distance: Double) {
 
-    override fun run() {
-        while(opMode.opModeIsActive()) {
-            if(Math.abs(deployingSpeed) > Math.abs(climbingSpeed)) {
-                if(!deployMotorBrake) {
-                    deploymentMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-                    deployMotorBrake = true
-                }
-                deploymentMotor.rawPower = deployingSpeed
-                climbMotor.rawPower = 0.0
-                climbingSpeed = 0.0
-            }
-            else if(Math.abs(climbingSpeed) > Math.abs(deployingSpeed)) {
-                if(deployMotorBrake) {
-                    deploymentMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
-                    deployMotorBrake = false
-                }
-                climbMotor.rawPower = climbingSpeed
-                deploymentMotor.rawPower = 0.0
-                deployingSpeed = 0.0
-            }
-            else {
-                deploymentMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-                deploymentMotor.rawPower = 0.0
-                deployingSpeed = 0.0
-                climbMotor.rawPower = 0.0
-                climbingSpeed = 0.0
-            }
-        }
     }
+    isBusy 
+        get() = leftMotor.isBusy || rightMotor.isBusy
 }
