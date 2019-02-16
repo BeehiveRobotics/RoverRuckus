27bdd8d74ceaaea8904bbd161fa53294be0a0dd1
Kaden Gordon <kaden.m.gordon@gmail.com>
2019-02-16 10:36:31 -0700

    Testing new land() function for updates to robot

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/AutoParts.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/AutoParts.kt
@@ -4,17 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.BeehiveRobotics.Library.Util.BROpMode
 import org.firstinspires.ftc.teamcode.CV.GoldMineralPosition
 
-//@Autonomous(name="Testing auto", group="RR2")
+@Autonomous(name="Testing auto", group="RR2")
 class AuroParts(): BROpMode(BROpMode.OpModeType.Autonomous) {
     private val robot = Robot(this)
     override fun initialize() {
         robot.init()
+        robot.cv.startCV()
+        robot.cv.stopCV()        
     }
     override fun onStartPressed() {
         robot.start()
-        robot.cv.startCV()
-        robot.cv.stopCV()
+        robot.lift.unlock()
     }
     override fun run() {
+        robot.land()
     }
 }
