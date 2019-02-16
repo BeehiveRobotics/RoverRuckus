8c24defa405245c6e37fb2ab429a9f39ceb75f88
Kaden Gordon <kaden.m.gordon@gmail.com>
2019-02-16 10:36:49 -0700

    Rewrote for new deployment mechanism

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Deployment.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Deployment.kt
@@ -7,26 +7,48 @@ import com.qualcomm.robotcore.hardware.Servo
 import org.BeehiveRobotics.Library.Sensors.REVColorSensor
 
 class Deployment(private val opMode: BROpMode): RobotSystem(opMode), Runnable {
-    val kickerServo = BRServo(opMode, "dks")
+    val topKicker = BRServo(opMode, "dtks")
+    val bottomKicker = BRServo(opMode, "dbks")
     private val rightFlipServo = BRServo(opMode, "drfs")
     private val leftFlipServo = BRServo(opMode, "dlfs")
     private val balanceServo = BRServo(opMode, "dbs")
-    val cs = REVColorSensor(opMode, "dcs")
+    val topCS = REVColorSensor(opMode, "dtcs")
+    val bottomCS = REVColorSensor(opMode, "dbcs")
     private val REVEALED_POSITION = 0.0
-    private val STOWED_POSITION = 1.0
+    private val STOWED_POSITION = 0.65
     private val UP_POSITION = 0.8
-    private val RIGHT_POSITION = 1.0
+    private val RIGHT_POSITION = 0.75
     private val MIDDLE_POSITION = 0.5
-    private val LEFT_POSITION = 0.0
+    private val LEFT_POSITION = 0.25
     private val A_LITTLE_OFF_FROM_THE_MIDDLE_POSITION = 0.51
     private val OUT_OF_THE_WAY_FOR_LANDING_POSITION = 0.7
     private var isUp = false
     private val kickerPosition = A_LITTLE_OFF_FROM_THE_MIDDLE_POSITION
     private val KICK_DELAY_TIME = 400L
+    private val COLOR_SENSOR_THRESHOLD = 0.35
     enum class Tasks {
-        KICK_RIGHT, KICK_LEFT, REVEAL, NONE
+        KICK_RIGHT_RIGHT, KICK_LEFT_LEFT, KICK_LEFT_RIGHT, KICK_RIGHT_LEFT, REVEAL, NONE
+    }
+    enum class KickDirection {
+        RIGHT, LEFT
     }
     private var task = Tasks.NONE
+    internal var flipPosition: Double = STOWED_POSITION
+        internal set(value) {
+            rightFlipServo.position = value + 0.1
+            leftFlipServo.position = value + 0.1
+            balanceServo.position = value
+            field = value
+        }
+    /*This is only for testing servo positions */
+    internal var kickersPosition: Double = MIDDLE_POSITION
+        internal set(value) {
+            topKicker.position = value
+            bottomKicker.position = value
+            field = value
+        }
+
+
 
     override fun init() {
         rightFlipServo.direction = Servo.Direction.REVERSE
@@ -53,39 +75,71 @@ class Deployment(private val opMode: BROpMode): RobotSystem(opMode), Runnable {
         }
     }
     fun knock() {
-        
+        val topCSRGB = topCS.RGB()
+        val bottomCSRGB = bottomCS.RGB()
+        val topNumber = (topCSRGB[0]-topCSRGB[2]).toFloat()/topCSRGB.max()!!.toFloat()
+        val bottomNumber = (bottomCSRGB[0]-bottomCSRGB[2]).toFloat()/bottomCSRGB.max()!!.toFloat()
+        val topKickDirection = if(topNumber<COLOR_SENSOR_THRESHOLD) KickDirection.LEFT else KickDirection.RIGHT
+        val bottomKickDirection = if(bottomNumber<COLOR_SENSOR_THRESHOLD) KickDirection.LEFT else KickDirection.RIGHT
+        when(topKickDirection) {
+            KickDirection.LEFT -> {
+                when(bottomKickDirection) {
+                    KickDirection.LEFT -> kickLeftLeft()
+                    KickDirection.RIGHT -> kickLeftRight()
+                }
+            }
+            KickDirection.RIGHT -> {
+                when(bottomKickDirection) {
+                    KickDirection.LEFT -> kickRightLeft()
+                    KickDirection.RIGHT -> kickRightRight()
+                }
+            }
+        }
     }
 
     fun up() {
         setFlipPosition(UP_POSITION)
     }
 
-    fun right() {
-        task = Tasks.KICK_RIGHT
+    fun kickRightRight() {
+        task = Tasks.KICK_RIGHT_RIGHT
         val thread = Thread(this)
         thread.start()
     }
 
-    fun left() {
-        task = Tasks.KICK_LEFT
+    fun kickLeftLeft() {
+        task = Tasks.KICK_LEFT_LEFT
+        val thread = Thread(this)
+        thread.start()
+    }
+    
+    fun kickRightLeft() {
+        task = Tasks.KICK_RIGHT_LEFT
+        val thread = Thread(this)
+        thread.start()
+    }
+
+    fun kickLeftRight() {
+        task = Tasks.KICK_LEFT_RIGHT
         val thread = Thread(this)
         thread.start()
     }
 
     private fun middle() {
-        setKickerPosition(A_LITTLE_OFF_FROM_THE_MIDDLE_POSITION)
+        setKickerPositions(MIDDLE_POSITION, MIDDLE_POSITION)
     }
 
-    private fun setKickerPosition(position: Double) {
-        kickerServo.position = position
+    private fun setKickerPositions(topPosition: Double, bottomPosition: Double) {
+        topKicker.position = topPosition
+        bottomKicker.position = bottomPosition
     }
 
     private fun setFlipPosition(position: Double) {
-        rightFlipServo.position = position
-        leftFlipServo.position = position
-        balanceServo.position = position
+        rightFlipServo.position = position + 0.35
+        leftFlipServo.position = position + 0.35
+        balanceServo.position = (position + 0.35) * 0.85
     }
-
+    //Fix toString()
     override fun toString(): String =
         "Kicker Position: ${if(kickerPosition==REVEALED_POSITION) "Up" else if(kickerPosition==STOWED_POSITION) "Stowed" else "Something else???"} \n" + 
         "Deploying Position: ${if(isUp) "Out" else "In"}"
@@ -95,29 +149,41 @@ class Deployment(private val opMode: BROpMode): RobotSystem(opMode), Runnable {
         when(task) {
             Tasks.REVEAL -> {
                 setFlipPosition(REVEALED_POSITION)
-                sleep(250)
-                setKickerPosition(A_LITTLE_OFF_FROM_THE_MIDDLE_POSITION)
-                sleep(750)
-                setKickerPosition(MIDDLE_POSITION)
                 isUp = true
                 task = Tasks.NONE
             }
-            Tasks.KICK_RIGHT -> {
+            Tasks.KICK_RIGHT_RIGHT -> {
                 if(isUp) {
-                    setKickerPosition(RIGHT_POSITION)
+                    setKickerPositions(RIGHT_POSITION, RIGHT_POSITION)
                     sleep(KICK_DELAY_TIME)
                     middle()
                 }
                 task = Tasks.NONE
             }
-            Tasks.KICK_LEFT -> {
+            Tasks.KICK_LEFT_LEFT -> {
                 if(isUp) {
-                    setKickerPosition(LEFT_POSITION)
+                    setKickerPositions(LEFT_POSITION, LEFT_POSITION)
                     sleep(KICK_DELAY_TIME)
                     middle()
                 }
                 task = Tasks.NONE
             }
+            Tasks.KICK_LEFT_RIGHT -> {
+                if(isUp) {
+                    setKickerPositions(LEFT_POSITION, RIGHT_POSITION)
+                    sleep(KICK_DELAY_TIME)
+                    middle()
+                }
+                task = Tasks.NONE
+            }
+            Tasks.KICK_RIGHT_LEFT -> {
+                if(isUp) {
+                    setKickerPositions(RIGHT_POSITION, LEFT_POSITION)
+                    sleep(KICK_DELAY_TIME)
+                    middle()
+                }
+                task = Tasks.NONE
+            } 
         }
     }
 
