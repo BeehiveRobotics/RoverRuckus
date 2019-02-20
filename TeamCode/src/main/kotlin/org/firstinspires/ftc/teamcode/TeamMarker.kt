b8f11b3982934292922d88e0667a785279f86d77
Kaden Gordon <kaden.m.gordon@gmail.com>
2019-02-20 15:49:56 -0700

    Update TeamMarker for new attaching

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/TeamMarker.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/TeamMarker.kt
@@ -7,8 +7,8 @@ import org.BeehiveRobotics.Library.Systems.RobotSystem
 class TeamMarker(private val opMode: BROpMode): RobotSystem(opMode) {
     private val servo = Servo(opMode, "tms")
     private val UP_POSITION = 0.5
-    private val OUT_OF_THE_WAY_POSITION = 0.45
-    private val DOWN_POSITION = 0.0
+    private val OUT_OF_THE_WAY_POSITION = 0.7
+    private val DOWN_POSITION = 1.0
     fun up() {
         setServoPosition(UP_POSITION)
     }
