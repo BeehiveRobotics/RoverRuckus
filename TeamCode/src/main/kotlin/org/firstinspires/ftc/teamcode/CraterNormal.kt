25652d41d36d81c271bfa0b83a63efdbeea2f0eb
Kaden Gordon <kaden.m.gordon@gmail.com>
2019-02-20 15:47:09 -0700

    Finishing up the two main autonomous programs

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/CraterNormal.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/CraterNormal.kt
@@ -10,57 +10,49 @@ class CraterNormal(): BROpMode(BROpMode.OpModeType.Autonomous) {
     private val robot = Robot(this)
     override fun initialize() {
         robot.init()
+        robot.cv.startCV()
     }
     override fun onStartPressed() {
         robot.start()
-        robot.cv.startCV()
     }
     override fun run() {
         robot.land()
-        val runTime = ElapsedTime()
         val goldMineralPosition = robot.cv.getGoldMineralPosition()
         robot.cv.stopCV()
-        robot.drive.forward(1.0, 14.0, false)
-        sleep(500)
-        robot.lift.runToPosition(1.0, 2000.0, false) //NEEDS CHANGED
-        robot.waitUntilNotBusy()
+        robot.drive.spinRight(0.3, 1.0)
+        robot.drive.forward(1.0, 10.0, waitForCompletion = false)
+        sleep(150)
+        robot.lift.runToPosition(-1.0, 2500.0, waitForCompletion = false)
+        robot.drive.waitUntilNotBusy()
         when(goldMineralPosition) {
-            GoldMineralPosition.LEFT -> {
-                robot.drive.strafeLeft(1.0, 16.0)
-            }
-            GoldMineralPosition.CENTER -> {
-                robot.drive.strafeRight(1.0, 6.0)
-            }
-            GoldMineralPosition.RIGHT -> {
-                robot.drive.strafeRight(1.0, 24.0)
-            }
+            GoldMineralPosition.LEFT ->   robot.drive.strafeLeft(0.7, 16.0)
+            GoldMineralPosition.CENTER -> robot.drive.strafeRight(0.4, 4.0)
+            GoldMineralPosition.RIGHT ->  robot.drive.strafeRight(0.7, 16.0)
         }
-        robot.drive.forward(1.0, 12.0)
-        robot.drive.backward(1.0, 10.0)
+        robot.drive.forward(0.75, 14.0)   
+        robot.drive.backward(0.75, 7.0)
         when(goldMineralPosition) {
-            GoldMineralPosition.LEFT -> {
-                robot.drive.strafeLeft(1.0, 24.0)
-            }
-            GoldMineralPosition.CENTER -> {
-                robot.drive.strafeLeft(1.0, 46.0)
-            }
-            GoldMineralPosition.RIGHT -> {
-                robot.drive.strafeLeft(1.0, 64.0)
-            }
+            GoldMineralPosition.LEFT ->   robot.drive.strafeLeft(0.75, 24.0)
+            GoldMineralPosition.CENTER -> robot.drive.strafeLeft(0.75, 50.0)
+            GoldMineralPosition.RIGHT ->  robot.drive.strafeLeft(0.75, 64.0)
         }
-        robot.drive.rightGyro(1.0, -135.0)
-        robot.drive.backward(0.8, 12.0)
-        robot.drive.forward(0.8, 1.5)
-        if(goldMineralPosition == GoldMineralPosition.RIGHT) robot.drive.forward(1.0, 8.0)
-        robot.drive.strafeRight(1.0, 50.0)
+        robot.drive.spinLeft(0.7, 10.0)
+        robot.drive.forward(0.6)
+        sleep(750L)
+        robot.drive.stopMotors()
+        robot.drive.backward(0.8, 1.0)
+        robot.drive.strafeLeft(0.85, 42.0)
         robot.teamMarker.down()
-        robot.drive.strafeLeft(1.0, 56.0, false)
+        robot.drive.spinLeft(1.0, 1.5)
+        robot.drive.strafeRight(0.85, 45.0, waitForCompletion = false)
         sleep(500)
         robot.teamMarker.up()
         robot.waitUntilNotBusy()
+        robot.drive.spinRight(1.0, 20.0)
+        robot.drive.strafeLeft(0.6, 10.0)
+        robot.drive.forward(1.0, 24.0, waitForCompletion = false)
         robot.gathering.inBetween()
-        robot.drive.leftGyro(1.0, -45.0)
-        robot.drive.forward(1.0, 16.0, false)
+        sleep(350)
         robot.gathering.inOutMotor.runForTime(-1.0, 500L)
         robot.gathering.down()
         robot.waitUntilNotBusy()
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/DepotNormal.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/DepotNormal.kt
@@ -9,58 +9,50 @@ class DepotNormal(): BROpMode(BROpMode.OpModeType.Autonomous) {
     private val robot = Robot(this)
     override fun initialize() {
         robot.init()
+        robot.cv.startCV()
     }
     override fun onStartPressed() {
         robot.start()
-        robot.cv.startCV()
     }
     override fun run() {
         robot.land()
         val goldMineralPosition = robot.cv.getGoldMineralPosition()
         robot.cv.stopCV()
-        robot.drive.forward(1.0, 14.0, false)
-        sleep(500)
-        robot.lift.runToPosition(1.0, 2000.0, false) //NEEDS CHANGED
-        robot.waitUntilNotBusy()
+        robot.drive.spinRight(0.3, 1.0)
+        robot.drive.forward(1.0, 10.0, waitForCompletion = false)
+        sleep(150)
+        robot.lift.runToPosition(-1.0, 2500.0, waitForCompletion = false)
+        robot.drive.waitUntilNotBusy()
         when(goldMineralPosition) {
-            GoldMineralPosition.LEFT -> {
-                robot.drive.strafeLeft(1.0, 16.0)
-            }
-            GoldMineralPosition.CENTER -> {
-                robot.drive.strafeRight(1.0, 6.0)
-            }
-            GoldMineralPosition.RIGHT -> {
-                robot.drive.strafeRight(1.0, 24.0)
-            }
+            GoldMineralPosition.LEFT ->   robot.drive.strafeLeft(0.7, 16.0)
+            GoldMineralPosition.CENTER -> robot.drive.strafeRight(0.4, 4.0)
+            GoldMineralPosition.RIGHT ->  robot.drive.strafeRight(0.7, 16.0)
         }
-        robot.drive.forward(1.0, 12.0)
-        robot.drive.backward(1.0, 12.0)
+        robot.drive.forward(0.75, 14.0)   
+        robot.drive.backward(0.75, 8.0)
         when(goldMineralPosition) {
-            GoldMineralPosition.LEFT -> {
-                robot.drive.strafeLeft(1.0, 24.0)
-            }
-            GoldMineralPosition.CENTER -> {
-                robot.drive.strafeLeft(1.0, 46.0)
-            }
-            GoldMineralPosition.RIGHT -> {
-                robot.drive.strafeLeft(1.0, 64.0)
-            }
+            GoldMineralPosition.LEFT ->   robot.drive.strafeLeft(0.75, 24.0)
+            GoldMineralPosition.CENTER -> robot.drive.strafeLeft(0.75, 50.0)
+            GoldMineralPosition.RIGHT ->  robot.drive.strafeLeft(0.75, 64.0)
         }
-        //robot.drive.rightGyro(1.0, -135.0)
-        robot.drive.leftGyro(1.0, 30.0) //was 45.0
-        //robot.drive.backward(1.0, 12.0)
-        robot.drive.forward(1.0, 12.0)
-        //robot.drive.forward(1.0, 3.0)
-        //robot.drive.rightGyro(1.0, 50.0)
-        //robot.drive.forward(1.0, 4.0)
-        robot.drive.strafeRight(1.0, 46.0)
+        robot.drive.spinRight(1.0, 30.0)
+        robot.drive.backward(1.0)
+        sleep(750L)
+        robot.drive.stopMotors()
+        robot.drive.strafeLeft(0.75, 52.0)
         robot.teamMarker.down()
-        robot.drive.strafeLeft(1.0, 46.0, false) //was 56.0
-        sleep(500)
+        robot.drive.spinRight(1.0, 2.0)
+        robot.drive.strafeRight(0.75, 46.0, false) //was 56.0
+        sleep(750)
         robot.teamMarker.up()
         robot.waitUntilNotBusy()
-        robot.drive.leftGyro(1.0, 130.0)
-        robot.drive.forward(1.0, 16.0, false)
+        robot.drive.spinRight(1.0, 24.0)
+        robot.drive.strafeRight(1.0)
+        sleep(350L)
+        robot.drive.stopMotors()
+        robot.drive.forward(1.0, 24.0, false)
+        robot.gathering.inBetween()
+        sleep(350)
         robot.gathering.inOutMotor.runForTime(-1.0, 500L)
         robot.gathering.down()
         robot.waitUntilNotBusy()
